477fe69cec01c0af73391161cb4d8778736211b6
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-23 18:06:11 +0900

    Implement `make_solution_code`

--- scripts/common.bash
+++ scripts/common.bash
@@ -53,6 +53,51 @@ function translate_file_name() {
   fi
 }
 
+# Generates the content of a Swift solution file for a given coding problem.
+#
+# Arguments:
+#   $1 - question_id: The unique number of the problem.
+#   $2 - question_name: The name or title of the problem.
+#   $3 - question_platform: The URL to the problem.
+#   $4 - ps_site_name: The name of the problem-solving site or platform.
+#   $5 - swift_code: The Swift code to solve the problem.
+#
+# Returns:
+#   The generated content of the Swift solution file.
+function make_solution_code() {
+  local question_id=$1
+  local question_name=$2
+  local question_platform=$3
+  local ps_site_name=$4
+  local swift_code=$5
+  local nickname
+  if [ -n "$NICKNAME" ] && [ -f "$NICKNAME" ]; then
+    nickname=$NICKNAME
+  else
+    nickname=Unknown
+  fi
+  local content
+  content=$(
+    cat <<EOF
+//
+//  $question_id. $question_name.swift
+//  $question_platform
+//  Algorithm
+//
+//  Created by $nickname on $(date "+%Y/%m/%d").
+//
+
+import Foundation
+
+final class $ps_site_name$question_id {
+  $swift_code
+}
+
+EOF
+  )
+  echo "$content"
+}
+
 # Add the Swift file to Xcode project
 function add_to_xcode_project() {
   local solution_file="$1"
